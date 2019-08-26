cee3c21c41a33952940e90dc33299a0170dd359b
Iqbal_Mubarok <muhammadiqbalmubarok@gmail.com>
2019-08-26 12:07:09 +0700

    Initial commit

---
+++ app/src/main/java/com/example/latihan1recyclecardview/SongListItem.java
@@ -0,0 +1,13 @@
+package com.example.latihan1recyclecardview;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SongListItem extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_song_list_item);
+    }
+}
